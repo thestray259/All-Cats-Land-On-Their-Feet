c913adac3fd3591cef0f88f0c8743c921b2bee1f
rileybazan115 <80065065+rileybazan115@users.noreply.github.com>
2023-02-01 20:23:04 -0700

    test

--- Assets/Scripts/Checkpoint/CheckpointSingle.cs
+++ Assets/Scripts/Checkpoint/CheckpointSingle.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CheckpointSingle : MonoBehaviour
 {
+	//test
 	private CourseCheckpoints courseCheckpoints;
 	private MeshRenderer meshRenderer;
 	[SerializeField] AudioSource audioSource;
